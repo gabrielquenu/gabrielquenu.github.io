1d793e2f0f13db0214e147a9190a8d251437bdfa
Gabriel Q. Nunes <gabrielq.nunes@luizalabs.com>
2022-09-30 14:53:54 -0300

    Visual animations developed

--- expanding-cards/script/script.js
+++ expanding-cards/script/script.js
@@ -1,4 +1,4 @@
-const galleryItemElements = document.querySelectorAll(".gallery-item")
+const galleryItemElements = document.querySelectorAll(".gallery-item>img")
 
 galleryItemElements.forEach((element) => {
     element.onclick = function () { setActiveInactive(element) }
@@ -7,12 +7,12 @@ galleryItemElements.forEach((element) => {
 function setActiveInactive(elementClicked) {
     galleryItemElements.forEach((element) => {
         if (elementClicked === element) {
-            element.classList.toggle('gallery-item')
-            element.classList.toggle('gallery-item-selected')
+            element.parentElement.classList.toggle('gallery-item')
+            element.parentElement.classList.toggle('gallery-item-selected')
         } else {
-            if (element.classList.contains('gallery-item-selected')) {
-                element.classList.add('gallery-item')
-                element.classList.remove('gallery-item-selected')
+            if (element.parentElement.classList.contains('gallery-item-selected')) {
+                element.parentElement.classList.add('gallery-item')
+                element.parentElement.classList.remove('gallery-item-selected')
             }
         }
     })
